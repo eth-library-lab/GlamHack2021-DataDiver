e01aac37bbeed3bb904ff00f0399c220a15cd9eb
barry <barrysunderland@outlook.com>
2021-04-17 01:41:59 +0200

    add flask boiler plat

--- flask_app/flask_app.py
+++ flask_app/flask_app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request, render_template
 import numpy as np
 
-app = Flask(__name__, template_folder='./templates',static_folder='./static')
+app = Flask(__name__, template_folder='templates', static_folder='./static')
 
 @app.route('/')
 def home():
